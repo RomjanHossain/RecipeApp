492d7cd83b1b30991bdc24b66dfb8e1efcede2ba
RomjanHossain <romjanhossain726526@gmail.com>
2020-03-23 16:50:25 +0600

    adding my own usermodel

--- app/loli/models.py
+++ app/loli/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserModel(BaseUserManager):
+    """ Creates and saves a new user """
+    
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Customer model using email instead of username """
+    email = models.EmailField(max_length=500, unique=True)
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserModel()
+
+    USERNAME_FIELD = 'email'
+

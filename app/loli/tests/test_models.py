f1761eddd2c51af77962a42646da823f4bf91531
RomjanHossain <romjanhossain726526@gmail.com>
2020-03-25 06:55:33 +0600

    testing email is normalize or not

--- app/loli/tests/test_models.py
+++ app/loli/tests/test_models.py
@@ -13,3 +13,11 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+    
+    def test_usermodel(self):
+        """testing if user email is normalize """
+        email = "romjanhossain@GMAILfuFKJ.Com"
+        user = get_user_model().objects.create_user(email,'hello1312')
+        # print(f'This is user email = {user.email}')
+        self.assertEqual(user.email, email.lower())
+        

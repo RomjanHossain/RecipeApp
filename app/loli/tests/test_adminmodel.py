b2825af3f832f0c30b07859a71d89ef54bbab02d
RomjanHossain <romjanhossain726526@gmail.com>
2020-03-25 11:37:10 +0600

    edited test_adminmodel

--- app/loli/tests/test_adminmodel.py
+++ app/loli/tests/test_adminmodel.py
@@ -8,20 +8,27 @@ class AdminTest(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@gmail.com',
-            password='whoCares,1232'
+            email='admin@londonappdev.com',
+            password='password123'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='whocares@email.com',
-            password='N-word5',
-            name='User Name'
+            email='test@londonappdev.com',
+            password='password123',
+            name='Test user full name'
         )
 
-    def test_user_listed(self):
-        """ Test that user are listed or not """
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
         url = reverse('admin:core_user_changelist')
-        response = self.client.get(url)
+        res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+    
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)

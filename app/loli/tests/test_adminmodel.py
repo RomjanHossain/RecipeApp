303130892abed3c6da0197dde197f09d17a3abf2
RomjanHossain <romjanhossain726526@gmail.com>
2020-03-25 20:00:16 +0600

    huh! done with that test

--- app/loli/tests/test_adminmodel.py
+++ app/loli/tests/test_adminmodel.py
@@ -20,7 +20,7 @@ class AdminTest(TestCase):
 
     def test_users_listed(self):
         """Test that users are listed on user page"""
-        url = reverse('admin:core_user_changelist')
+        url = reverse('admin:loli_user_changelist')
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
@@ -28,7 +28,14 @@ class AdminTest(TestCase):
     
     def test_user_change_page(self):
         """Test that the user edit page works"""
-        url = reverse('admin:core_user_change', args=[self.user.id])
+        url = reverse('admin:loli_user_change', args=[self.user.id])
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:loli_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
